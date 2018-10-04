f9ff816136e893f8cba9414b01c0c0aad1b49e7d
Arai Takashi <takashi.arai.nz@icloud.com>
2018-10-05 00:59:30 +0900

    WIP

---
+++ Makefile
@@ -0,0 +1,5 @@
+#!/usr/bin/make -f
+
+
+server:
+	php -S localhost:8000 public/index.php
--- src/App.php
+++ src/App.php
@@ -2,11 +2,46 @@
 
 namespace Ctrb;
 
+use ChatWork\OAuth2\Client\ChatWorkProvider;
+use League\OAuth2\Client\Grant\AuthorizationCode;
+use League\OAuth2\Client\Token\AccessToken;
+
 class App
 {
+
+    public $accessToken =<<<TOKEN
+****
+TOKEN;
+
+
     public function execute()
     {
-        echo "Hello!";
+
+        $provider = new ChatWorkProvider(
+            '****',
+            '****',
+            "****"
+        );
+
+        $url = $provider->getAuthorizationUrl();
+
+        echo $url;
+
+        if (isset($_GET['code'])) {
+            $token = $provider->getAccessToken(new AuthorizationCode(), [
+                'code' => $_GET['code']
+            ]);
+
+            echo '<hr>';
+
+            echo $token;
+        }
+
+        $owner = $provider->getResourceOwner(new AccessToken(['access_token' => $this->accessToken]));
+
+        echo '<hr>';
+
+        var_dump($owner);
     }
 }
 

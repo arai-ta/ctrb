fc777a5658c6d22bbce230dd1b7f9d6f83539b71
Arai Takashi <takashi.arai.nz@icloud.com>
2018-10-16 01:18:38 +0900

    WIP

--- public/index.php
+++ public/index.php
@@ -2,7 +2,16 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = new Ctrb\App;
+$env = new \Dotenv\Dotenv(__DIR__.'/..');
+$env->load();
 
+$logger = new \Monolog\Logger('CTRB');
+$logger->pushHandler(new \Monolog\Handler\StreamHandler(
+    __DIR__.'/../app.log'
+));
+
+$GLOBALS['logger'] = $logger;
+
+$app = new Ctrb\App;
 $app->execute();
 
--- src/App.php
+++ src/App.php
@@ -3,45 +3,96 @@
 namespace Ctrb;
 
 use ChatWork\OAuth2\Client\ChatWorkProvider;
+use ChatWork\OAuth2\Client\ChatWorkResourceOwner;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
 use League\OAuth2\Client\Grant\AuthorizationCode;
-use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Grant\RefreshToken;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
 class App
 {
 
-    public $accessToken =<<<TOKEN
-****
-TOKEN;
+    private $logger;
 
+    public function __construct($logger =  null)
+    {
+        $this->logger = $logger ?: $GLOBALS['logger'];
+    }
 
     public function execute()
     {
 
         $provider = new ChatWorkProvider(
-            '****',
-            '****',
-            "****"
+            getenv('CHATWORK_OAUTH_CLIENT_ID'),
+            getenv('CHATWORK_OAUTH_CLIENT_SECRET'),
+            getenv('CHATWORK_OAUTH_CLIENT_REDIRECT_URL')
         );
 
-        $url = $provider->getAuthorizationUrl();
+        $url = $provider->getAuthorizationUrl([
+            'scope' => [
+                'users.profile.me:read',
+                'users.tasks.me:read',
+            ]
+        ]);
 
         echo $url;
 
         if (isset($_GET['code'])) {
-            $token = $provider->getAccessToken(new AuthorizationCode(), [
-                'code' => $_GET['code']
-            ]);
 
-            echo '<hr>';
+            try {
+                $token = $provider->getAccessToken(new AuthorizationCode(), [
+                    'code' => $_GET['code']
+                ]);
 
-            echo $token;
+                echo '<hr>';
+
+                var_dump($token);
+            } catch (IdentityProviderException $e) {
+                echo '<hr>';
+                var_dump($e->getMessage());
+                $this->logger->debug('authenticationTokenの有効期限切れです');
+                exit('authenticationTokenの有効期限切れです');
+            }
+            $access_token = $token;
+        } else {
+            $access_token = $provider->getAccessToken(new RefreshToken(), ['refresh_token' => getenv('EXAMPLE_USER_REFRESH_TOKEN')]);
         }
-
-        $owner = $provider->getResourceOwner(new AccessToken(['access_token' => $this->accessToken]));
 
         echo '<hr>';
 
-        var_dump($owner);
+        var_dump($access_token->getToken());
+        if ($access_token->getExpires()) {
+            var_dump($access_token->getExpires());
+            var_dump($access_token->hasExpired());
+        } else {
+            var_dump('$access_token is not have "expires"');
+        }
+        var_dump($access_token->getRefreshToken());
+        var_dump($access_token->getResourceOwnerId());
+        var_dump($access_token->getValues());
+
+        echo '<hr>';
+        /** @var ChatWorkResourceOwner $owner */
+        $owner = $provider->getResourceOwner($access_token);
+        var_dump($owner->getName());
+
+        try {
+            $request = (new Request('GET', 'https://api.chatwork.com/v2/my/tasks'))
+                ->withHeader('Authorization', sprintf('Bearer %s', $access_token));
+
+            $client = new Client();
+
+            $response = $client->send($request);
+
+            var_dump($response->getStatusCode());
+            var_dump($response->getBody()->getContents());
+        } catch (ClientException $e) {
+            var_dump($e->getMessage());
+            exit;
+        }
+
     }
 }
 
